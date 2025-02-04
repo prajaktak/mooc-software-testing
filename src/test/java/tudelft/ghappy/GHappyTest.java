84d8bdf2fb4ebc19dcaad63395a6a29cece57ebd
prajaktakulkarni <prachee.j@gmail.com>
2025-02-04 12:51:16 +0100

    add Unit tests to GHappy class

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,27 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void testgHappyWithdoubleG() {
+        GHappy ghappy = new GHappy();
+        boolean result = ghappy.gHappy("xxggxx");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testgHappyWithSingleG() {
+        GHappy ghappy = new GHappy();
+        boolean result = ghappy.gHappy("xxgxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testgHappyWithDoubleAndSingleG() {
+        GHappy ghappy = new GHappy();
+        boolean result = ghappy.gHappy("xxggxxg");
+        Assertions.assertFalse(result);
+    }
 }
